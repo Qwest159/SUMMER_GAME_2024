d90c01fc8fd3d10019be6225c7dbff898fa4fe9b
Qwest159 <duchesne-raphael@hotmail.com>
2024-07-25 11:50:04 +0200

    affichage+actualisation

--- Pendu/functions/traitement_reponse.js
+++ Pendu/functions/traitement_reponse.js
@@ -1,12 +1,28 @@
-export function traitement_reponse_client(reponse, mot_a_trouver) {
+export function traitement_reponse_client(
+  reponse_client,
+  mot_a_trouver,
+  tableau_vrai,
+  tableau_faux,
+  vie
+) {
   console.log(mot_a_trouver);
-  if (mot_a_trouver.includes(reponse)) {
+
+  if (mot_a_trouver.includes(reponse_client)) {
     ////// si l'element se trouve dans le mot rechercher , alors rentre ici /////
-    console.log("OUI");
+    tableau_vrai.push(reponse_client);
+    console.log(tableau_vrai);
+    return tableau_vrai;
   } else {
     ////// si l'element NE SE TROUVE PAS dans le mot rechercher , alors rentre ici /////
-    console.log("NON");
-  }
+    tableau_faux.push(reponse_client);
 
-  //   return reponse;
+    vie = vierestante(vie, tableau_faux);
+    console.log(tableau_faux);
+    return tableau_faux;
+  }
+}
+
+// vie fonctionne PAS TOUCHE
+export function vierestante(vie, tableau_faux) {
+  return (vie = vie - tableau_faux.length);
 }
--- Pendu/main.js
+++ Pendu/main.js
@@ -1,13 +1,17 @@
 let mot = [];
-
 let vie = 7;
+let tableau_faux = [];
+let tableau_vrai = [];
 
 import {
   categorie_motchoisis,
   categorie_affichage,
 } from "./functions/categorie.js";
-import { affichage_pendu } from "./functions/affichage_pendu.js";
-import { traitement_reponse_client } from "./functions/traitement_reponse.js";
+import { affichage_pendu, reponse_pendu } from "./functions/affichage_pendu.js";
+import {
+  traitement_reponse_client,
+  vierestante,
+} from "./functions/traitement_reponse.js";
 document.querySelector("#app").innerHTML = `
 <h1> Choissisez la catégorie</h1>
 <p>Pour ce faire, vous devez écrire "aléatoire" ou bien, choisir l'une des catégories suivantes: ${categorie_affichage()}
@@ -27,12 +31,32 @@ envoier.addEventListener("click", () => {
   if (categorie_motchoisis()) {
     const mot = categorie_motchoisis();
     const mot_tableau = [...mot];
-    affichage_pendu(mot_tableau, vie);
-    let reponseclient = document.querySelector("#reponse_utilisateur_lettre");
+    affichage_pendu(mot_tableau, vierestante(vie, tableau_faux));
+
     let envoieclient = document.querySelector("#envoier_lettre");
+    console.log("cliciqze");
+    //////COMMENCEMENT DU JEU /////////
     envoieclient.addEventListener("click", () => {
+      let reponseclient = document.querySelector("#reponse_utilisateur_lettre");
       if (reponseclient.value) {
-        affichage(traitement_reponse_client(reponseclient.value, mot_tableau));
+        affichage(
+          traitement_reponse_client(
+            reponseclient.value,
+            mot_tableau,
+            tableau_vrai,
+            tableau_faux,
+            vie
+          ),
+          "h5"
+        );
+        document.getElementById("mot").innerHTML = reponse_pendu(mot_tableau);
+        document.getElementById("vierestante").innerHTML =
+          "Vie restante: " + vierestante(vie, tableau_faux);
+        if (vierestante(vie, tableau_faux) == 0) {
+          document.querySelector("#app").innerHTML = `
+          <h1>GAME OVER</h1>
+          `;
+        }
       } else {
         affichage("Mot mal écrit", "h3");
       }
