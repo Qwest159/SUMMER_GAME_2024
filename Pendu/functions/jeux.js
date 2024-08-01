81dbd45e8525303e87143a037d0d85aa49937f47
Qwest159 <duchesne-raphael@hotmail.com>
2024-08-01 17:31:05 +0200

    terminer

--- Pendu/functions/jeux.js
+++ Pendu/functions/jeux.js
@@ -116,20 +116,20 @@ export function jeu_pendu() {
     <p>Le mot a trouvé: <strong>${mot}</strong></p>
     <div id="imggroupe">
       <figure id="groupe1">
-        <img class="fusee" src="../storage/image/fusée.png" alt="fusée" />
-        <img class="explosion" src="../storage/image/explosion.png" alt="fusée" />
+        <img class="fusee" src="./image/fusée.png" alt="fusée" />
+        <img class="explosion" src="./image/explosion.png" alt="fusée" />
       </figure>
       <figure id="groupe2">
-        <img id="fusee" src="../storage/image/fusee2centrale.png" alt="fusée" />
+        <img id="fusee" src="./image/fusee2centrale.png" alt="fusée" />
         <img
           id="explosion"
-          src="../storage/image/explo2centrale.png"
+          src="./image/explo2centrale.png"
           alt="fusée"
         />
       </figure>
       <figure id="groupe3">
-        <img class="fusee" src="../storage/image/fusee3droite.png" alt="fusée" />
-        <img class="explosion" src="../storage/image/explosion.png" alt="fusée" />
+        <img class="fusee" src="./image/fusee3droite.png" alt="fusée" />
+        <img class="explosion" src="./image/explosion.png" alt="fusée" />
       </figure>
  
     </main>
